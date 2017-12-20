4692730ff01c5590a7b12161b29e27075ce56e1c
Mikołaj Sadek <sadekm@student.mini.pw.edu.pl>
2017-12-20 07:45:15 +0100

    Resolving inconsistecies, removing duplicates

--- MSI_Project/MSI_Project/RoughSetExpert.cpp
+++ MSI_Project/MSI_Project/RoughSetExpert.cpp
@@ -7,15 +7,27 @@
 
 using node = std::tuple<std::string, int, char>;
 
-RoughSetExpert::RoughSetExpert() : ExpertInterface{}
+RoughSetExpert::RoughSetExpert() : ExpertInterface{},
+    Input{}, Questions{}, Table{}, Decisions{}, IndistinguishableMatrix{},
+    Inconsistencies{}
 {
-    Answers = Questions = std::vector<node>();
-    Table = std::vector<std::vector<int>>();
+    //Input = Questions = std::vector<node>();
+    //Table = IndistinguishableMatrix = std::vector<std::vector<int>>();
     if (!ReadTable()) {
         throw std::exception("Invalid table");
     }
     GetMaxVals();
     FillDecisions();
+    std::cout << "Wczytywanie danych systemu eksperckiego zakoñczone.\n";
+    Setup();
+}
+
+void RoughSetExpert::Prepare()
+{
+    FindInconsistencies();
+    if (Inconsistencies.size() != 0)
+        GetRidOfInconsistency();
+    RemoveDuplicates();
     FindReduct();
 }
 
@@ -55,7 +67,7 @@ bool RoughSetExpert::ReadTable()
         getline(ss, val, ',');
         val = val.substr(1);
         val.pop_back();
-        Answers.emplace_back(std::make_tuple(val, a++, 0));
+        Input.emplace_back(std::make_tuple(val, a++, 0));
         int qi = 0;
         while (getline(ss, val, ',')) {
             Table[qi].emplace_back(stoi(val));
@@ -80,7 +92,7 @@ void RoughSetExpert::GetMaxVals()
 
 void RoughSetExpert::FillDecisions()
 {
-    for (auto& a : Answers) {
+    for (auto& a : Input) {
         auto Text = std::get<std::string>(a);
         auto it = std::find(Decisions.begin(), Decisions.end(), Text);
         if (it != Decisions.end())
@@ -88,6 +100,154 @@ void RoughSetExpert::FillDecisions()
         Decisions.push_back(Text);
         ++Nd;
     }
+}
+
+void RoughSetExpert::Setup()
+{
+    std::cout << "Wpisz 'v' aby program wyœwietla³ szczegó³owe informacje lub 'q', aby uzyskaæ jedynie wynik.\n";
+    std::string v;
+    std::cin >> v;
+    while (v != "v" && v != "V" && v != "q" && v != "Q") {
+        std::cout << "Niezrozumia³a odpowiedŸ, spróbuj jeszcze raz." << std::endl;
+        std::cin >> v;
+    }
+    verbose = (v == "v" || v == "V");
+    v = "";
+    std::cout << "Wybierz metodê usuniêcia ewentualnych niespójnoœci z danych: 1 - metoda jakoœciowa, 2 - metoda uogólnionego argumentu\n";
+    std::cin >> v;
+    while (v != "1" && v != "2") {
+        std::cout << "Niezrozumia³a odpowiedŸ, spróbuj jeszcze raz." << std::endl;
+        std::cin >> v;
+    }
+    if (v == "1")
+        GetRidOfInconsistency = [this] { this->DeleteLowerScore(); };
+    else
+        GetRidOfInconsistency = [this] { this->Generalize(); };
+}
+
+void RoughSetExpert::FindInconsistencies()
+{
+    for (int i = 0; i < Na; ++i) {
+        std::vector<int> sameRows{ i };
+        std::vector<int> duplicates{};
+        if (std::find_if(Inconsistencies.begin(), Inconsistencies.end(), [i](auto& v) { return std::find(v.begin(), v.end(), i) != v.end(); }) != Inconsistencies.end())
+            continue;
+        for (int j = i + 1; j < Na; ++j) {
+            bool same = true;
+            for (int q = 0; q < Nq; ++q) {
+                if (Table[q][i] != Table[q][j]) {
+                    same = false;
+                    break;
+                }
+            }
+            if (same) {
+                if (std::get<std::string>(Input[i]) != std::get<std::string>(Input[j]))
+                    sameRows.push_back(j);
+                else
+                    duplicates.push_back(j);
+            }
+        }
+        if (sameRows.size() > 1) {
+            if (duplicates.size() > 0)
+                sameRows.insert(sameRows.end(), duplicates.begin(), duplicates.end());
+            Inconsistencies.emplace(std::move(sameRows));
+        }
+    }
+}
+
+void RoughSetExpert::Generalize()
+{
+    for (auto& vec : Inconsistencies) {
+        std::string general;
+        for (int i : vec) {
+            if (general.size())
+                general += " lub ";
+            general += std::get<std::string>(Input[i]);
+        }
+        for (int i : vec) {
+            std::get<std::string>(Input[i]) = general;
+        }
+    }
+    Decisions.clear();
+    FillDecisions();
+}
+
+void RoughSetExpert::DeleteLowerScore()
+{
+    for (auto& vec : Inconsistencies) {
+        std::set<std::string> Different{};
+        for (int i = 0; i < vec.size(); ++i)
+            Different.insert(std::get<std::string>(Input[vec[i]]));
+        int maxL = 0;
+        int maxU = 0;
+        std::string L, U, W;
+        for (auto& x : Different) {
+            int lower = LowerBound(x);
+            int upper = UpperBound(x);
+            if (lower > maxL) {
+                maxL = lower;
+                L = x;
+            }
+            if (upper > maxU) {
+                maxU = upper;
+                U = x;
+            }
+        }
+        if (L == U)
+            W = U;
+        else
+            W = maxL > maxU ? L : U;
+        // remove from input and table those rows that are in this vec, but are not decided to be W
+        // we will mark rows to delete by setting decision as empty string, and actually remove later
+        for (int i = 0; i < vec.size(); ++i) {
+            if (W == std::get<std::string>(Input[vec[i]]))
+                continue;
+            std::get<std::string>(Input[vec[i]]) = "";
+        }
+    }
+
+    // now remove rows with empty strings
+    for (int i = Na - 1; i >= 0; --i) {
+        if (std::get<std::string>(Input[i]) != "")
+            continue;
+        Input.erase(Input.begin() + i);
+        for (auto& col : Table)
+            col.erase(col.begin() + i);
+        --Na;
+    }
+
+    // now update indices
+    for (int i = 0; i < Na; ++i)
+        std::get<int>(Input[i]) = i;
+}
+
+void RoughSetExpert::RemoveDuplicates()
+{
+    for (int i = 0; i < Na; ++i) {
+        std::vector<int> duplicates{};
+        for (int j = i + 1; j < Na; ++j) {
+            bool same = true;
+            for (int q = 0; q < Nq; ++q) {
+                if (Table[q][i] != Table[q][j]) {
+                    same = false;
+                    break;
+                }
+            }
+            if (same) {
+                if (std::get<std::string>(Input[i]) == std::get<std::string>(Input[j]))
+                    duplicates.push_back(j);
+            }
+        }
+        for (int d = duplicates.size() - 1; d >= 0; --d) {
+            Input.erase(Input.begin() + duplicates[d]);
+            for (auto& col : Table)
+                col.erase(col.begin() + duplicates[d]);
+        }
+        Na -= duplicates.size();
+    }
+    // now update indices
+    for (int i = 0; i < Na; ++i)
+        std::get<int>(Input[i]) = i;
 }
 
 void RoughSetExpert::FindReduct()
@@ -108,4 +268,34 @@ void RoughSetExpert::ComputeResult()
 
 void RoughSetExpert::ShowResult()
 {
+}
+
+int RoughSetExpert::LowerBound(const std::string& x)
+{
+    int count = 0;
+    for (auto& row : Input) {
+        if (std::get<std::string>(row) != x)
+            continue;
+        int i = std::get<int>(row);
+        if (std::find_if(Inconsistencies.begin(), Inconsistencies.end(), [i](auto& v) { return std::find(v.begin(), v.end(), i) != v.end(); }) != Inconsistencies.end())
+            continue;
+        ++count;
+    }
+    return count;
+}
+
+int RoughSetExpert::UpperBound(const std::string& x)
+{
+    int count = 0;
+    for (auto& row : Input) {
+        if (std::get<std::string>(row) != x)
+            continue;
+        int i = std::get<int>(row);
+        auto vi = std::find_if(Inconsistencies.begin(), Inconsistencies.end(), [i](auto& v) { return std::find(v.begin(), v.end(), i) != v.end(); });
+        if (vi != Inconsistencies.end())
+            count += vi->size();
+        else;
+            ++count;
+    }
+    return count;
 }
--- MSI_Project/MSI_Project/RoughSetExpert.h
+++ MSI_Project/MSI_Project/RoughSetExpert.h
@@ -2,30 +2,47 @@
 
 #include "ExpertInterface.h"
 
+#include <functional>
 #include <tuple>
 #include <string>
 #include <map>
 #include <vector>
+#include <set>
 
 class RoughSetExpert : public ExpertInterface {
 public:
     RoughSetExpert();
+    void Prepare();
     void Run() override;
 private:
     // tuple is a node consisting of text, index, and maxval
     // (maxval is uchar, we assume minval for all questions to be 0)
-    std::vector<std::tuple<std::string, int, char>> Answers;
+    std::vector<std::tuple<std::string, int, char>> Input;
     std::vector<std::tuple<std::string, int, char>> Questions;
     std::vector<std::string> Decisions;
     std::vector<std::vector<int>> Table;
+    std::vector<std::vector<int>> IndistinguishableMatrix;
+    std::set<std::vector<int>> Inconsistencies;
     int Na, Nq, Nd;
+    bool verbose;
 
     bool ReadTable();
     void GetMaxVals();
     void FillDecisions();
+    void Setup();
+
+    void FindInconsistencies();
+    std::function<void(void)> GetRidOfInconsistency;
+    void Generalize();
+    void DeleteLowerScore();
+    void RemoveDuplicates();
     void FindReduct();
+
     void Greet();
     void Ask();
     void ComputeResult();
     void ShowResult();
+
+    int LowerBound(const std::string&);
+    int UpperBound(const std::string&);
 };
